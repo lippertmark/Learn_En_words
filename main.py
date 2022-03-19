34d204e368ba968f746eb481996fc03e235252cf
lippertmark <lippert.mark.d@gmail.com>
2022-03-20 01:19:08 +0300

    add tasks

--- main.py
+++ main.py
@@ -5,21 +5,39 @@ bot = telebot.TeleBot('тут будет апи ключ')
 
 
 def sql_new_user(tg_id, tg_username):
-    # TODO сделать запись в БД
+    # TODO(@Сергей) сделать запись в БД
     pass
 
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     pass
-    # TODO написать приветственное сообщение
-    # TODO сделать меню
-    # TODO обратиться к функции которую сделает @Сергей для создания пользоваетля sql_new_user(tg_id, tg_username)
+    # TODO(@Олеся) написать приветственное сообщение
+    # TODO(@Олеся) сделать меню
+    # про меню:
+    # кнопка СЛОВА: callback_data='words'
+    # кнопка ПРОФИЛЬ: callback_data='profile'
+    # TODO(@Олеся) обратиться к функции которую сделает @Сергей для создания пользоваетля sql_new_user(tg_id, tg_username)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    pass
+    username = call.message.chat.username
+    if call.message:
+        if call.data == 'words':
+            pass
+            # TODO(@Амир) создать меню типа УЧИТЬ или Повторять
+            # описание меню:
+            # кнопка УЧИТЬ: callback_data='learn'
+            # кнопка ПОВТОРЯТЬ: callback_data='repeat'
+        elif call.data == 'words':
+            pass
+            # TODO(@Амир) организовать выдачу слов
+            # пока хз как
+
+
+
+
     # тут ответы на кнопки
 
 
