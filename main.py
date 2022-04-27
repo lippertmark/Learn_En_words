5d4dac012ce824a1a4f8c70a6caff6ec486ea019
lippertmark <lippert.mark.d@gmail.com>
2022-04-28 00:19:48 +0300

    all is merged and good

--- main.py
+++ main.py
@@ -2,58 +2,106 @@ import telebot
 from telebot import types
 from sql.use_sql import *
 
+import random
+
 bot = telebot.TeleBot('5162531568:AAFulbpqupsSMHiri53UD0jIRC7gpzUayTc')
+
+
+def is_learned(tg_id, word_id):
+    '''
+    Проверяет слово выучено пользователем или нет.
+    :param tg_id:
+    :param word_id:
+    :return: true/false
+    '''
+    # TODO (@Олеся)
+    if sql_notes_by_user_and_word(tg_id, word_id) != []:  # надо не как с классом
+        return True
+    return False
+
+
+def generate_word(tg_id):
+    '''
+    Генерирует новое слово, проверяя его на то что оно уже выучено, если выучено то генерируется новое.
+    :param tg_id:
+    :return: word (tuple): (word_id, word_en, word_ru, category, sentance, hate)
+    '''
+    # TODO (@Олеся)
+    word = random.choice(sql_all_words())
+    if is_learned(tg_id, word.word_id):  # надо не как с классом
+        generate_word(tg_id)
+    else:
+        return word
+
+
+def send_new_word(tg_id):
+    '''
+    Генерирует новое слово
+    Отправляет юзеру это слово
+    :param tg_id:
+    :return:
+    '''
+    # TODO (@Олеся) модификация не больше 10 слов
+    word = generate_word(tg_id)
+    bot.send_message(chat_id=tg_id.from_user.id, text=f'{word.word_en}')  # добавить предложение
+    new_note(tg_id, word_id, GENERATED, None)
+    # TODO (@Олеся) добавить инлайнкейборд для выбора правильного варианта
 
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     user_id = message.from_user.id
     user_username = message.from_user.username
-# TODO(@Олеся) написать приветственное сообщение
     sticker = open('img/welcome.webp', 'rb')
     bot.send_sticker(message.chat.id, sticker)
-     # в общем, у меня плохо с речью и приветствиями,
-    # я накалякаю, а потом исправим на нормальное приветствие
-# TODO(@Олеся) сделать меню
-    # про меню:
-    # кнопка СЛОВА: callback_data='words'
-    # кнопка ПРОФИЛЬ: callback_data='profile'
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Профиль')
-    item2 = types.KeyboardButton('Учить новые слова')
-    item3 = types.KeyboardButton('Повторять слова')
+    markup = telebot.types.InlineKeyboardMarkup()
+    item1 = telebot.types.InlineKeyboardButton(text='Профиль🗂', callback_data='profile')
+    item2 = telebot.types.InlineKeyboardButton(text='Учить новые слова🔎', callback_data='learn_new')
+    item3 = telebot.types.InlineKeyboardButton(text='Повторять слова📚', callback_data='repeat_words')
 
     markup.add(item1, item2, item3)
-    bot.send_message(message.chat.id, 'Привет, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот для изучения английского языка.'.format(
+    bot.send_message(message.chat.id,
+                     'Привет, {0.first_name}!🥰\nЯ - <b>{1.first_name}</b>, бот для изучения английского языка.🤖'.format(
                          message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
-
-    # TODO(@Олеся) обратиться к функции которую сделает @Сергей для создания пользоваетля sql_new_user(tg_id, tg_username)
     sql_new_user(user_id, user_username)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    username = call.message.chat.username
-    if call.message:
-        if call.data == 'words':
-            pass
-            # TODO(@Амир) создать меню типа УЧИТЬ или Повторять
-            # описание меню:
-            # кнопка УЧИТЬ: callback_data='learn'
-            # кнопка ПОВТОРЯТЬ: callback_data='repeat'
-        elif call.data == 'words':
-            pass
-            # TODO(@Амир) организовать выдачу слов
-            # пока хз как
+    if sql_is_user_in_db(call.message.from_user.id):
+        username = call.message.chat.username
+        # TODO (@Олеся) проверка на существование юзера
+        if call.message:
+            if call.data == 'profile':
+                markup2 = telebot.types.InlineKeyboardMarkup()
+                markup2.add(telebot.types.InlineKeyboardButton(text='Учить новые слова🔎', callback_data='learn_new'))
+                markup2.add(telebot.types.InlineKeyboardButton(text='Повторять слова📚', callback_data='repeat_words'))
 
-
-
-
-    # тут ответы на кнопки
+                achive = '✅' * sql_user_info(call.message.from_user.id).score  # надо не как с классом
+                bot.send_message(call.message.chat.id,
+                                 f'Ваш ник: {username}\n\nАктивность за 10 дней: {achive}\n\nВыученных слов: {sql_user_info(call.message.from_user.id).cnt_words_total}',
+                                 reply_markup=markup2)
+            elif call.data == 'learn_new':
+                # TODO (@Олеся) вызввать нфункцию
+                pass
+            elif call.data == 'repeat_words':
+                pass
+            elif call.data == 'wrong':
+                # TODO (@Олеся) сделать обработчик вронг, отправка слова
+                pass
+            elif call.data == 'accept':
+                # TODO (@Олеся) сообщение похвала, new_note(tg_id, word_id, RETRY, 0), отправка нового слова
+                # TODO (@Олеся) инкриминировать счетчик выученных слов
+                pass
+        # тут ответы на кнопки
 
 
 @bot.message_handler(content_type=['text'])
 def text(message):
-    pass
-    # тут ответы на текст
+    if sql_is_user_in_db(message.from_user.id):
+        # TODO (@Олеся) проверка на существование юзера
+        pass
+        # тут ответы на текст
+
 
 bot.polling(none_stop=True)
